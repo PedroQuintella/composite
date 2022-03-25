f3e7201b632b68445a2604e8bbcd17b425a3d679
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-03-25 16:34:07 -0300

    Criação das classes dos objetos e da classe abstrata

---
+++ src/main/java/Exercito.java
@@ -0,0 +1,15 @@
+public class Exercito {
+
+    private Grupamento corpoMilitar;
+
+    public void setCorpoMilitar(Grupamento corpoMilitar) {
+        this.corpoMilitar = corpoMilitar;
+    }
+
+    public String getCorpoMilitar() {
+        if (this.corpoMilitar == null) {
+            throw new NullPointerException("Exército sem corpo militar");
+        }
+        return this.corpoMilitar.getGrupamento();
+    }
+}
